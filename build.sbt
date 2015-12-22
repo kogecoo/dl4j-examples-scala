78eb7a9ecc8c45bb68a42f517f962cfbe3ea035b
Octavian Geagla <ogeagla@gmail.com>
2015-12-22 11:10:30 -0700

    update dependencies and usage examples

--- build.sbt
+++ build.sbt
@@ -6,16 +6,18 @@ scalaVersion := "2.10.4"
 
 lazy val root = project.in(file("."))
 
+val nd4jVersion = "0.4-rc3.7"
+val dl4jVersion =	"0.4-rc3.7"
+val canovaVersion = "0.0.0.13"
+val jacksonVersion = "2.5.1"
+
 libraryDependencies ++= Seq(
-  "commons-io" % "commons-io" % "2.4",
-  "com.google.guava" % "guava" % "18.0",
-  "org.deeplearning4j" % "deeplearning4j-core" % "0.4-rc3.4",
-  "org.deeplearning4j" % "deeplearning4j-nlp" % "0.4-rc3.4",
-  "org.deeplearning4j" % "deeplearning4j-ui" % "0.4-rc3.4",
-  "org.jblas" % "jblas" % "1.2.4",
-  "org.nd4j" % "canova-nd4j-image" % "0.0.0.11",
-  "org.nd4j" % "nd4j-jblas" % "0.4-rc3.5",
-  "org.nd4j" % "nd4j-x86" % "0.4-rc3.5"
+  "org.deeplearning4j" % "deeplearning4j-core" % dl4jVersion,
+  "org.deeplearning4j" % "deeplearning4j-nlp" % dl4jVersion,
+  "org.deeplearning4j" % "deeplearning4j-ui" % dl4jVersion,
+  "org.nd4j" % "canova-nd4j-image" % canovaVersion,
+  "org.nd4j" % "canova-nd4j-codec" % canovaVersion,
+  "org.nd4j" % "nd4j-x86" % nd4jVersion
 )
 
 resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
--- src/main/scala/org/deeplearning4j/examples/convolution/LenetMnistExample.scala
+++ src/main/scala/org/deeplearning4j/examples/convolution/LenetMnistExample.scala
@@ -1,126 +1,122 @@
 package org.deeplearning4j.examples.convolution
 
+import java.util.Random
+
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator
 import org.deeplearning4j.eval.Evaluation
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
-import org.deeplearning4j.nn.conf.{ MultiLayerConfiguration, NeuralNetConfiguration }
-import org.deeplearning4j.nn.conf.layers.{ ConvolutionLayer, DenseLayer, OutputLayer, SubsamplingLayer }
 import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup
+import org.deeplearning4j.nn.conf.layers.{ConvolutionLayer, DenseLayer, OutputLayer, SubsamplingLayer}
+import org.deeplearning4j.nn.conf.{MultiLayerConfiguration, NeuralNetConfiguration, Updater}
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
-import org.deeplearning4j.optimize.api.IterationListener
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.deeplearning4j.ui.weights.HistogramIterationListener
 import org.nd4j.linalg.api.buffer.DataBuffer
 import org.nd4j.linalg.api.ndarray.INDArray
-import org.nd4j.linalg.dataset.DataSet
-import org.nd4j.linalg.dataset.SplitTestAndTrain
-import org.nd4j.linalg.dataset.api.iterator.{ DataSetIterator, MultipleEpochsIterator }
+import org.nd4j.linalg.dataset.api.iterator.{DataSetIterator, MultipleEpochsIterator}
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.lossfunctions.LossFunctions
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import java.util.ArrayList
-import java.util.List
-import java.util.Random
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.mutable
 
 
 object LenetMnistExample {
 
-    lazy val log: Logger = LoggerFactory.getLogger(LenetMnistExample.getClass)
+  lazy val log: Logger = LoggerFactory.getLogger(LenetMnistExample.getClass)
 
-    def main(args: Array[String]) = {
-        Nd4j.dtype = DataBuffer.Type.DOUBLE
-        Nd4j.factory().setDType(DataBuffer.Type.DOUBLE)
-        Nd4j.ENFORCE_NUMERICAL_STABILITY = true
+  def main(args: Array[String]) = {
+    Nd4j.dtype = DataBuffer.Type.DOUBLE
+    Nd4j.factory().setDType(DataBuffer.Type.DOUBLE)
+    Nd4j.ENFORCE_NUMERICAL_STABILITY = true
 
-        val nChannels = 1
-        val outputNum = 10
-        val numSamples = 60000
-        val batchSize = 500
-        val iterations = 1
-        val splitTrainNum = (batchSize*.8).toInt
-        val seed = 123
-        val listenerFreq = iterations/5
-        val testInputBuilder = mutable.ArrayBuilder.make[INDArray]
-        val testLabelsBuilder = mutable.ArrayBuilder.make[INDArray]
+    val nChannels = 1
+    val outputNum = 10
+    val numSamples = 60000
+    val batchSize = 500
+    val iterations = 1
+    val splitTrainNum = (batchSize*.8).toInt
+    val seed = 123
+    val listenerFreq = iterations/5
+    val testInputBuilder = mutable.ArrayBuilder.make[INDArray]
+    val testLabelsBuilder = mutable.ArrayBuilder.make[INDArray]
 
-        log.info("Load data....")
-        val mnistIter: DataSetIterator = new MultipleEpochsIterator(5,new MnistDataSetIterator(batchSize,numSamples, true))
+    log.info("Load data....")
+    val mnistIter: DataSetIterator = new MultipleEpochsIterator(5,new MnistDataSetIterator(batchSize,numSamples, true))
 
-        log.info("Build model....")
-        val builder: MultiLayerConfiguration.Builder = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .batchSize(batchSize)
-                .iterations(iterations).useDropConnect(true)
-                .constrainGradientToUnitNorm(true).regularization(true)
-                .l2(2e-3)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .list(6)
-                .layer(0, new ConvolutionLayer.Builder(5, 5)
-                        .nIn(nChannels)
-                        .nOut(20).dropOut(0.5)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation("relu")
-                        .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, Array[Int](2, 2))
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(5, 5)
-                        .nIn(nChannels)
-                        .nOut(50)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation("relu")
-                        .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, Array[Int](2, 2))
-                        .build())
-                  .layer(4,new DenseLayer.Builder().activation("tanh")
-                          .nOut(500).build())
-                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nOut(outputNum)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation("softmax")
-                        .build())
-                .backprop(true).pretrain(false)
+    log.info("Build model....")
+    val builder = new NeuralNetConfiguration.Builder()
+      .seed(seed)
+      .iterations(iterations)
+      .regularization(true).l2(0.0005)
+      .learningRate(0.01)
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+      .updater(Updater.NESTEROVS).momentum(0.9)
+      .list(6)
+      .layer(0, new ConvolutionLayer.Builder(5, 5)
+        .nIn(nChannels)
+        .stride(1, 1)
+        .nOut(20).dropOut(0.5)
+        .weightInit(WeightInit.XAVIER)
+        .activation("relu")
+        .build())
+      .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, Array[Int](2, 2))
+        .build())
+      .layer(2, new ConvolutionLayer.Builder(5, 5)
+        .nIn(20)
+        .nOut(50)
+        .stride(2,2)
+        .weightInit(WeightInit.XAVIER)
+        .activation("relu")
+        .build())
+      .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, Array[Int](2, 2))
+        .build())
+      .layer(4, new DenseLayer.Builder().activation("tanh")
+        .nOut(500).build())
+      .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+        .nOut(outputNum)
+        .weightInit(WeightInit.XAVIER)
+        .activation("softmax")
+        .build())
+      .backprop(true).pretrain(false);
 
-        new ConvolutionLayerSetup(builder,28,28,1)
+    new ConvolutionLayerSetup(builder,28,28,1)
 
-        val conf: MultiLayerConfiguration = builder.build()
+    val conf: MultiLayerConfiguration = builder.build()
 
-        val model: MultiLayerNetwork = new MultiLayerNetwork(conf)
-        model.init()
+    val model: MultiLayerNetwork = new MultiLayerNetwork(conf)
+    model.init()
 
-        log.info("Train model....")
-        model.setListeners(new ScoreIterationListener(listenerFreq), new HistogramIterationListener(1))
-        while(mnistIter.hasNext()) {
-            val mnist = mnistIter.next()
-            val trainTest = mnist.splitTestAndTrain(splitTrainNum, new Random(seed)); // train set that is the result
-            val trainInput = trainTest.getTrain(); // get feature matrix and labels for training
-            testInputBuilder += trainTest.getTest().getFeatureMatrix()
-            testLabelsBuilder += trainTest.getTest().getLabels()
+    log.info("Train model....")
+    model.setListeners(new ScoreIterationListener(listenerFreq), new HistogramIterationListener(1))
+    while(mnistIter.hasNext()) {
+      val mnist = mnistIter.next()
+      val trainTest = mnist.splitTestAndTrain(splitTrainNum, new Random(seed)); // train set that is the result
+      val trainInput = trainTest.getTrain(); // get feature matrix and labels for training
+      testInputBuilder += trainTest.getTest().getFeatureMatrix()
+      testLabelsBuilder += trainTest.getTest().getLabels()
 
-            model.fit(trainInput)
-        }
-
-        val testInput = testInputBuilder.result
-        val testLabels = testLabelsBuilder.result
-
-        log.info("Evaluate weights....")
-
-        log.info("Evaluate model....")
-        val eval = new Evaluation(outputNum)
-        testInput.zip(testLabels).foreach { case (input, label) =>
-            val output = model.output(input)
-            eval.eval(label, output)
-        }
-        val output = model.output(testInput.head)
-        eval.eval(testLabels.head, output)
-        log.info(eval.stats())
-        log.info("****************Example finished********************")
-
-
+      model.fit(trainInput)
     }
+
+    val testInput = testInputBuilder.result
+    val testLabels = testLabelsBuilder.result
+
+    log.info("Evaluate weights....")
+
+    log.info("Evaluate model....")
+    val eval = new Evaluation(outputNum)
+    testInput.zip(testLabels).foreach { case (input, label) =>
+      val output = model.output(input)
+      eval.eval(label, output)
+    }
+    val output = model.output(testInput.head)
+    eval.eval(testLabels.head, output)
+    log.info(eval.stats())
+    log.info("****************Example finished********************")
+
+
+  }
 
 }
