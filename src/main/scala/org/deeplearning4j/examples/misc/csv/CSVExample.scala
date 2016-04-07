9c70359b1c3fd9a69d642045faefd289c3b3a192
kogecoo <kogecoo@gmail.com>
2016-04-07 17:55:22 +0900

    sync with upstream (2dc5f9da6299efe621c87018f4e24435592ca52b)

--- src/main/scala/org/deeplearning4j/examples/misc/csv/CSVExample.scala
+++ src/main/scala/org/deeplearning4j/examples/misc/csv/CSVExample.scala
@@ -35,8 +35,8 @@ object CSVExample {
         recordReader.initialize(new FileSplit(new ClassPathResource("iris.txt").getFile))
 
         //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
-        val labelIndex = 4
-        val numClasses = 3
+        val labelIndex = 4     //5 values in each row of the iris.txt CSV: 4 input features followed by an integer label (class) index. Labels are the 5th value (index 4) in each row
+        val numClasses = 3     //3 classes (types of iris flowers) in the iris data set. Classes have integer values 0, 1 or 2
         val batchSize = 150    //Iris data set: 150 examples total. We are loading all of them into one DataSet (not recommended for large data sets)
         val iterator: DataSetIterator = new RecordReaderDataSetIterator(recordReader,batchSize,labelIndex,numClasses);
 
